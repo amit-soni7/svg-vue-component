d973ab1a658689cb3adab16ab79daa0e10179580
Amit Kumar Soni <amit.soni7@gmail.com>
2023-04-01 12:11:44 +0530

    new builds

--- dist/svg-vue.esm.js
+++ dist/svg-vue.esm.js
@@ -1,14 +1,13 @@
-import { computed, openBlock, createBlock, mergeProps } from 'vue';
+import { computed, openBlock, createElementBlock, mergeProps } from 'vue';
 
 var script = {
   name: 'SvgVue',
   props: {
     icon: String
   },
-
   setup(props) {
     const iconPath = computed(() => props.icon.replace(new RegExp('.'.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1'), 'g'), '/') + '.svg');
-    const svgString = computed(() => require(`svg-files-path/${iconPath.value}`).default);
+    const svgString = computed(() => require(`${iconPath.value}`).default);
     const svgAttributes = computed(() => {
       if (!svgString) return {};
       let wrapper = document.createElement('div');
@@ -24,32 +23,35 @@ var script = {
       svgContent
     };
   }
-
 };
 
+const _hoisted_1 = ["innerHTML"];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return openBlock(), createBlock("svg", mergeProps($setup.svgAttributes, {
+  return openBlock(), createElementBlock("svg", mergeProps($setup.svgAttributes, {
     innerHTML: $setup.svgContent
-  }), null, 16, ["innerHTML"]);
+  }), null, 16, _hoisted_1);
 }
 
 script.render = render;
 
 // Import vue component
+
+// Default export is installable instance of component.
 // IIFE injects install function into component, allowing component
 // to be registered via Vue.use() as well as Vue.component(),
-
 var entry_esm = /*#__PURE__*/(() => {
   // Get component instance
-  const installable = script; // Attach install function executed by Vue.use()
+  const installable = script;
 
+  // Attach install function executed by Vue.use()
   installable.install = app => {
     app.component('SvgVue', installable);
   };
-
   return installable;
-})(); // It's possible to expose named exports when writing components that can
+})();
+
+// It's possible to expose named exports when writing components that can
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
 // export const RollupDemoDirective = directive;
 
-export default entry_esm;
+export { entry_esm as default };
--- dist/svg-vue.ssr.js
+++ dist/svg-vue.ssr.js
@@ -1,38 +1,36 @@
-'use strict';var vue=require('vue');function _slicedToArray(arr, i) {
+'use strict';var vue=require('vue');function _iterableToArrayLimit(arr, i) {
+  var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"];
+  if (null != _i) {
+    var _s,
+      _e,
+      _x,
+      _r,
+      _arr = [],
+      _n = !0,
+      _d = !1;
+    try {
+      if (_x = (_i = _i.call(arr)).next, 0 === i) {
+        if (Object(_i) !== _i) return;
+        _n = !1;
+      } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0);
+    } catch (err) {
+      _d = !0, _e = err;
+    } finally {
+      try {
+        if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return;
+      } finally {
+        if (_d) throw _e;
+      }
+    }
+    return _arr;
+  }
+}
+function _slicedToArray(arr, i) {
   return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
 }
-
 function _arrayWithHoles(arr) {
   if (Array.isArray(arr)) return arr;
 }
-
-function _iterableToArrayLimit(arr, i) {
-  if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;
-  var _arr = [];
-  var _n = true;
-  var _d = false;
-  var _e = undefined;
-
-  try {
-    for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
-      _arr.push(_s.value);
-
-      if (i && _arr.length === i) break;
-    }
-  } catch (err) {
-    _d = true;
-    _e = err;
-  } finally {
-    try {
-      if (!_n && _i["return"] != null) _i["return"]();
-    } finally {
-      if (_d) throw _e;
-    }
-  }
-
-  return _arr;
-}
-
 function _unsupportedIterableToArray(o, minLen) {
   if (!o) return;
   if (typeof o === "string") return _arrayLikeToArray(o, minLen);
@@ -41,15 +39,11 @@ function _unsupportedIterableToArray(o, minLen) {
   if (n === "Map" || n === "Set") return Array.from(o);
   if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
 }
-
 function _arrayLikeToArray(arr, len) {
   if (len == null || len > arr.length) len = arr.length;
-
   for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
-
   return arr2;
 }
-
 function _nonIterableRest() {
   throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
 }var script = {
@@ -62,7 +56,7 @@ function _nonIterableRest() {
       return props.icon.replace(new RegExp('.'.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1'), 'g'), '/') + '.svg';
     });
     var svgString = vue.computed(function () {
-      return require("svg-files-path/".concat(iconPath.value)).default;
+      return require("".concat(iconPath.value)).default;
     });
     var svgAttributes = vue.computed(function () {
       if (!svgString) return {};
@@ -83,33 +77,36 @@ function _nonIterableRest() {
       svgContent: svgContent
     };
   }
-};function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return vue.openBlock(), vue.createBlock("svg", vue.mergeProps($setup.svgAttributes, {
+};var _hoisted_1 = ["innerHTML"];
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return vue.openBlock(), vue.createElementBlock("svg", vue.mergeProps($setup.svgAttributes, {
     innerHTML: $setup.svgContent
-  }), null, 16, ["innerHTML"]);
+  }), null, 16, _hoisted_1);
 }script.render = render;// Import vue component
+
+// Default export is installable instance of component.
 // IIFE injects install function into component, allowing component
 // to be registered via Vue.use() as well as Vue.component(),
-
 var component = /*#__PURE__*/(function () {
   // Get component instance
-  var installable = script; // Attach install function executed by Vue.use()
+  var installable = script;
 
+  // Attach install function executed by Vue.use()
   installable.install = function (app) {
     app.component('SvgVue', installable);
   };
-
   return installable;
-})(); // It's possible to expose named exports when writing components that can
+})();
+
+// It's possible to expose named exports when writing components that can
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
 // export const RollupDemoDirective = directive;
-var namedExports=/*#__PURE__*/Object.freeze({__proto__:null,'default': component});// only expose one global var, with named exports exposed as properties of
+var namedExports=/*#__PURE__*/Object.freeze({__proto__:null,'default':component});// Attach named exports directly to component. IIFE/CJS will
+// only expose one global var, with named exports exposed as properties of
 // that global var (eg. plugin.namedExport)
-
 Object.entries(namedExports).forEach(function (_ref) {
   var _ref2 = _slicedToArray(_ref, 2),
-      exportName = _ref2[0],
-      exported = _ref2[1];
-
+    exportName = _ref2[0],
+    exported = _ref2[1];
   if (exportName !== 'default') component[exportName] = exported;
 });module.exports=component;
